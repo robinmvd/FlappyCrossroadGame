48a1106f01a6707f4e07cd289f287bf59eb93927
Robin Michael van Doorn <robinmvdoorn@gmail.com>
2020-06-29 18:33:24 +0200

    Add collision check and score.

--- dev/game.ts
+++ dev/game.ts
@@ -1,25 +1,61 @@
 class Game {
     private div: HTMLElement
-    
-    private player:Player
-    
-    constructor() {    
+
+    private gameobjects : GameObject[] = []
+
+    private score : number = 0
+    private scoreElement: HTMLElement
+
+    constructor() {  
+        
         this.div = document.createElement("level")
         document.body.appendChild(this.div)
+
+        for (let i = 0; i < 4; i++) {
+            this.gameobjects.push(new Car())
+        }
     
-        this.player = new Player()
+        this.gameobjects.push(new Player(this))
+
+        this.scoreElement = document.createElement("score")
+        this.scoreElement.innerHTML = "Score: 0"
+        this.div.appendChild(this.scoreElement)
 
         this.gameLoop()
     }
 
     private gameLoop(){
-        this.player.update()
 
+        for (const gameobject of this.gameobjects) {
+
+            gameobject.update()
+
+            if(gameobject instanceof Player) {
+                
+                for (const car of this.gameobjects) {
+                    if(car instanceof Car) {
+
+                        if (this.checkCollision(car.getRectangle(), gameobject.getRectangle())) {
+                            console.log("crash!")
+                            gameobject.reset()
+                        }
+
+                    }
+                }
+            }
+            
+        }
+                
         requestAnimationFrame(this.gameLoop.bind(this));
     }
 
     public update() : void {
         
+    }
+
+    public addPoint() : void {
+        this.score++
+        this.scoreElement.innerHTML = "Score: "+this.score
     }
 
     private checkCollision(a: ClientRect, b: ClientRect) {
